3f6ea3ccd93461278a09a5c96b85a58e5669ed46
Trịnh Thiên <84885182+tdthien106202@users.noreply.github.com>
2024-01-01 00:49:05 +0700

    Frist commit

--- test.py
+++ test.py
@@ -12,36 +12,109 @@ from model import RFDNNet
 from utils import *
 from tensorflow.keras import Model, Input
 
+import tkinter as tk
+from tkinter import filedialog
 
-def run(config, model):
-    for name in os.listdir(config.test_path):
-        fullname = os.path.join(config.test_path, name)
-        lr = cv2.imread(fullname)
-        out, out_bilinear = upscale_image(model, lr)
-        cv2.imwrite(os.path.join(fullname.replace('.png', '_sr.png')), np.array(out))
-        cv2.imwrite(os.path.join(fullname.replace('.png', '_bilinear.png')), np.array(out_bilinear))
+class GUI:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("RFDNNet Parameters")
+
+        # Test Path
+        self.test_path_label = tk.Label(root, text="Test Path:")
+        self.test_path_label.grid(row=0, column=0, padx=10, pady=10)
+        self.test_path_entry = tk.Entry(root, width=40)
+        self.test_path_entry.grid(row=0, column=1, padx=10, pady=10)
+        self.browse_test_path_button = tk.Button(root, text="Browse", command=self.browse_test_path)
+        self.browse_test_path_button.grid(row=0, column=2, padx=10, pady=10)
+
+        # GPU
+        self.gpu_label = tk.Label(root, text="GPU:")
+        self.gpu_label.grid(row=1, column=0, padx=10, pady=10)
+        self.gpu_entry = tk.Entry(root, width=40)
+        self.gpu_entry.grid(row=1, column=1, padx=10, pady=10)
+
+        # Weight Test Path
+        self.weight_test_path_label = tk.Label(root, text="Weight Test Path:")
+        self.weight_test_path_label.grid(row=2, column=0, padx=10, pady=10)
+        self.weight_test_path_entry = tk.Entry(root, width=40)
+        self.weight_test_path_entry.grid(row=2, column=1, padx=10, pady=10)
+        self.browse_weight_test_path_button = tk.Button(root, text="Browse", command=self.browse_weight_test_path)
+        self.browse_weight_test_path_button.grid(row=2, column=2, padx=10, pady=10)
+
+        # RSAfilter
+        self.RSAfilter_label = tk.Label(root, text="RSAfilter:")
+        self.RSAfilter_label.grid(row=3, column=0, padx=10, pady=10)
+        self.RSAfilter_entry = tk.Entry(root, width=40)
+        self.RSAfilter_entry.grid(row=3, column=1, padx=10, pady=10)
+
+        # Filter
+        self.filter_label = tk.Label(root, text="Filter:")
+        self.filter_label.grid(row=4, column=0, padx=10, pady=10)
+        self.filter_entry = tk.Entry(root, width=40)
+        self.filter_entry.grid(row=4, column=1, padx=10, pady=10)
+
+        # Feat
+        self.feat_label = tk.Label(root, text="Feat:")
+        self.feat_label.grid(row=5, column=0, padx=10, pady=10)
+        self.feat_entry = tk.Entry(root, width=40)
+        self.feat_entry.grid(row=5, column=1, padx=10, pady=10)
+
+        # Scale
+        self.scale_label = tk.Label(root, text="Scale:")
+        self.scale_label.grid(row=6, column=0, padx=10, pady=10)
+        self.scale_entry = tk.Entry(root, width=40)
+        self.scale_entry.grid(row=6, column=1, padx=10, pady=10)
+
+        # Run Button
+        self.run_button = tk.Button(root, text="Run", command=self.run_model)
+        self.run_button.grid(row=10, column=1, pady=20)
+
+    def browse_test_path(self):
+        test_path = filedialog.askdirectory()
+        self.test_path_entry.delete(0, tk.END)
+        self.test_path_entry.insert(0, test_path)
+
+    def browse_weight_test_path(self):
+        weight_test_path = filedialog.askopenfilename(filetypes=[("H5 Files", "*.h5")])
+        self.weight_test_path_entry.delete(0, tk.END)
+        self.weight_test_path_entry.insert(0, weight_test_path)
+
+    def run_model(self):
+        # Get the values entered by the user
+        test_path = self.test_path_entry.get()
+        gpu = self.gpu_entry.get()
+        weight_test_path = self.weight_test_path_entry.get()
+        RSAfilter = int(self.RSAfilter_entry.get())
+        filter_val = int(self.filter_entry.get())
+        feat = int(self.feat_entry.get())
+        scale = int(self.scale_entry.get())
+
+        # Create a dictionary of parameters to pass to the main script
+        config = {
+            'test_path': test_path,
+            'gpu': gpu,
+            'weight_test_path': weight_test_path,
+            'RSAfilter': RSAfilter,
+            'filter': filter_val,
+            'feat': feat,
+            'scale': scale
+        }
+
+        # Create RFDNNet model
+        rfanet_x = RFDNNet()
+        x = Input(shape=(None, None, 3))
+        out = rfanet_x.main_model(x, 3)
+        rfa = Model(inputs=x, outputs=out)
+        rfa.summary()
+
+        # Load pre-trained weights
+        rfa.load_weights(config['weight_test_path'])
+
+        # Run the model
+        run(config, rfa)
 
 if __name__ == "__main__":
-    
-    parser = argparse.ArgumentParser()
-
-	# Input Parameters
-    parser.add_argument('--test_path', type=str, default="test/")
-    parser.add_argument('--gpu', type=str, default='1')
-    parser.add_argument('--weight_test_path', type=str, default= "weights/best.h5")
-    parser.add_argument('--RSAfilter', type=int, default=64)
-    parser.add_argument('--filter', type=int, default=64)
-    parser.add_argument('--feat', type=int, default=64)
-    parser.add_argument('--scale', type=int, default=3)
-
-    config = parser.parse_args()
-    os.environ['CUDA_VISIBLE_DEVICES'] = config.gpu
-
-    rfanet_x = RFDNNet()
-    x = Input(shape=(None, None, 3))
-    out = rfanet_x.main_model(x, 3)
-    rfa = Model(inputs=x, outputs=out)
-    rfa.summary()
-    rfa.load_weights(config.weight_test_path)
-
-    run(config, rfa)
+    root = tk.Tk()
+    gui = GUI(root)
+    root.mainloop()
